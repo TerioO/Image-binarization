33b8ac05083c00aa09ff4f85b6a27194d833860a
Andi <andisurdu23@gmail.com>
2023-05-22 20:08:10 +0300

    Removed initial src

--- binarization.js
+++ binarization.js
@@ -5,7 +5,7 @@ const THRESHOLD = document.querySelector(".set-threshold");
 THRESHOLD.value = 100;
 
 const IMG = new Image();
-IMG.src = "./assets/Sample1.jpg";
+IMG.src = "";
 
 const download_button = document.querySelector("#download-image");
 // The name of the downloaded file:
